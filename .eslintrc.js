d5b4d9bdebde2e788d50f944f0ac78f1e0376d64
Lior Frenkel <hotfur@gmail.com>
2018-06-12 16:28:21 +0200

    Prepare server

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:security/recommended'],
+  plugins: ['security'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 8,
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-console': 0,
+  },
+};
