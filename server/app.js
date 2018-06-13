cec7110bac048867bd0f84cbfc80bb73b59edafb
Lior Frenkel <hotfur@gmail.com>
2018-06-13 16:14:11 +0200

    Stop ignore client build folder

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json({ limit: config.get('http:request:limit') }));
 app.use(bodyParser.urlencoded({ extended: false, limit: config.get('http:request:limit') }));
 app.use(helmet());
-app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+app.use(express.static(path.join(__dirname, '..', 'client', 'build'), { maxAge: '30d' }));
 console.log(path.join(__dirname, '..', 'client', 'build'));
 // routes
 app.use('/', clientRouter);
