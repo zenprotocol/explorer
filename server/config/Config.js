9b97eba2d4a0190aaaf1fa62bb699f33ba7a959f
Lior Frenkel <hotfur@gmail.com>
2018-06-14 11:55:34 +0200

    Make sure NODE_ENV is set

--- server/config/Config.js
+++ server/config/Config.js
@@ -6,6 +6,8 @@ const nconf = require('nconf');
 function init() {
   nconf.argv().env('__'); // use __ for nested environment variables
   const environment = nconf.get('NODE_ENV') || 'development';
+  // make sure NODE_ENV is not undefined
+  process.env.NODE_ENV = environment;
   nconf.file(environment, path.join(__dirname, environment.toLowerCase() + '.json'));
   nconf.file('default', path.join(__dirname, 'default.json'));
 }
