5ef60ed02085a268b09d170823bcc2c0b6ac9278
Lior Frenkel <hotfur@gmail.com>
2018-06-18 14:54:13 +0200

    Move babel requires to own file

--- server/app.js
+++ server/app.js
@@ -1,26 +1,7 @@
 'use strict';
 
 // pre processing for react ssr
-require('ignore-styles');
-require('asset-require-hook')({
-  extensions: [
-    'jpg', 'jpeg', 'png', 'gif', 'bmp'
-  ],
-  name: '/static/media/[name].[hash:8].[ext]',
-  limit: 10000
-});
-require('asset-require-hook')({
-  extensions: [
-    'svg'
-  ],
-  name: '/static/media/[name].[hash:8].[ext]',
-});
-require('url-loader');
-require('file-loader');
-require('babel-register')({
-  ignore: [/(build|node_modules)/],
-  presets: ['env', 'react-app'],
-});
+require('./babel');
 
 const express = require('express');
 const logger = require('morgan');
---
+++ server/babel.js
@@ -0,0 +1,20 @@
+require('ignore-styles');
+require('asset-require-hook')({
+  extensions: [
+    'jpg', 'jpeg', 'png', 'gif', 'bmp'
+  ],
+  name: '/static/media/[name].[hash:8].[ext]',
+  limit: 10000
+});
+require('asset-require-hook')({
+  extensions: [
+    'svg'
+  ],
+  name: '/static/media/[name].[hash:8].[ext]',
+});
+require('url-loader');
+require('file-loader');
+require('babel-register')({
+  ignore: [/(build|node_modules)/],
+  presets: ['env', 'react-app'],
+});
