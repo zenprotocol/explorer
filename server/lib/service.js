f061f84960f956f066c347be9fc47e14864d4e6c
Lior Frenkel <hotfur@gmail.com>
2018-06-21 09:17:34 +0200

    Send all requests from one main function - add config

--- server/lib/service.js
+++ server/lib/service.js
@@ -1,7 +1,8 @@
 'use strict';
 
-const Config = require('../config/Config');
 const axios = require('axios');
+const Config = require('../config/Config');
+const NetworkError = require('../components/common/NetworkError');
 const request = axios.create({
   baseURL: Config.get('zp:node'),
 });
@@ -9,17 +10,49 @@ const request = axios.create({
 const Endpoints = {
   blocks: {
     info: '/blockchain/info',
-    block: 'blockchain/block?blockNumber=',
+    block: '/blockchain/block?blockNumber=',
   },
 };
 
+let globalMute = false;
+
+function sendHttpRequest(config) {
+  if(globalMute) {
+    return Promise.resolve({data: {}});
+  }
+
+  return request.request(config).catch((error) => {
+    throw new NetworkError(error);
+  });
+}
+
 module.exports = {
+  config: {
+    mute(mute) {
+      globalMute = mute;
+    },
+    isMuted() {
+      return globalMute;
+    },
+    setBaseUrl(url) {
+      request.defaults.baseURL = url;
+    },
+    setTimeout(timeout) {
+      request.defaults.timeout = timeout;
+    },
+  },
   blocks: {
     async getChainInfo() {
-      return request.get(Endpoints.blocks.info).then(response => response.data);
+      return sendHttpRequest({
+        url: Endpoints.blocks.info,
+        method: 'get',
+      }).then(response => response.data);
     },
     async getBlock(blockNumber) {
-      return request.get(Endpoints.blocks.block + blockNumber).then(response => response.data);
+      return sendHttpRequest({
+        url: Endpoints.blocks.block + blockNumber,
+        method: 'get',
+      }).then(response => response.data);
     },
   },
 };
