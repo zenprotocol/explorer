2dcea2fd464d45696ecba44f15ea80c275471477
Lior Frenkel <hotfur@gmail.com>
2018-06-26 19:18:53 +0200

    Use mobx useStaticRendering

--- server/components/client/clientRenderer.js
+++ server/components/client/clientRenderer.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import { useStaticRendering } from 'mobx-react';
 const path = require('path');
 const fs = require('fs');
 
+useStaticRendering(true);
 // import our main App component
 import App from '../../../src/App';
 module.exports = (req, res, next) => {
@@ -11,7 +13,9 @@ module.exports = (req, res, next) => {
   try {
     const htmlData = fs.readFileSync(filePath, { encoding: 'utf8' });
     // render the app as a string
-    const html = ReactDOMServer.renderToString(<App />);
+    const html = ReactDOMServer.renderToString(
+      <App />
+    );
 
     // inject the rendered app into our html and send it
     res.send(htmlData.replace('<div id="root"></div>', `<div id="root">${html}</div>`));
