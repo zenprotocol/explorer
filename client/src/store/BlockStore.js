c718d2d5608f9ba47e674aaa9d84c3247cbfdc18
Lior Frenkel <hotfur@gmail.com>
2018-06-26 12:26:16 +0200

    Comment out console log

--- client/src/store/BlockStore.js
+++ client/src/store/BlockStore.js
@@ -3,11 +3,11 @@ import {observable, decorate, action, autorun} from 'mobx';
 class BlockStore {
   blocks = [];
 
-  constructor() {
-    autorun(() => {
-      console.log('Blocks were changed', this.blocks);
-    });
-  }
+  // constructor() {
+  //   autorun(() => {
+  //     console.log('Blocks were changed', this.blocks);
+  //   });
+  // }
 
   setBlocks(blocks) {
     this.blocks = blocks;
