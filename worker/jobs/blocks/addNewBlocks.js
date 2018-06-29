017496fa2ebdddb864531ebc5895bc34522cf3bd
Lior Frenkel <hotfur@gmail.com>
2018-06-29 15:42:57 +0200

    Add block transactions

--- worker/jobs/blocks/addNewBlocks.js
+++ worker/jobs/blocks/addNewBlocks.js
@@ -2,7 +2,22 @@
 
 const Service = require('../../../server/lib/Service');
 const blocksDAL = require('../../../server/components/api/blocks/blocksDAL');
+const transactionsDAL = require('../../../server/components/api/transactions/transactionsDAL');
 const logger = require('../../lib/logger');
+
+/**
+ * Get a key from the job data object
+ *
+ * @param {Object} job
+ * @param {String} key
+ * @returns the key or null
+ */
+function getJobData(job, key) {
+  if (job && job.data && job.data[key]) {
+    return job.data[key];
+  }
+  return null;
+}
 
 async function getLatestBlockNumberFromNode() {
   const info = await Service.blocks.getChainInfo();
@@ -19,14 +34,46 @@ async function getLatestBlockNumberInDB() {
   return blockNumber;
 }
 
+async function createBlock(nodeBlock) {
+  logger.info(`Creating a new block with blockNumber ${nodeBlock.header.blockNumber}  ...`);
+  const block = await blocksDAL.create({
+    version: nodeBlock.header.version,
+    parent: nodeBlock.header.parent,
+    blockNumber: nodeBlock.header.blockNumber,
+    commitments: nodeBlock.header.commitments,
+    timestamp: nodeBlock.header.timestamp,
+    difficulty: nodeBlock.header.difficulty,
+    nonce1: nodeBlock.header.nonce[0],
+    nonce2: nodeBlock.header.nonce[1],
+  });
+  logger.info(`Block #${nodeBlock.header.blockNumber} created.`);
+  return block;
+}
+
+async function addTransactionToBlock(block, nodeTransaction, transactionHash) {
+  logger.info(`Creating a new transaction for block #${block.blockNumber}...`);
+  // create the transaction
+  const transaction = await transactionsDAL.create({
+    version: nodeTransaction.version,
+    hash: transactionHash,
+  });
+
+  logger.info('Transaction created.');
+
+  logger.info(`Adding the new transaction to block #${block.blockNumber}...`);
+  await blocksDAL.addTransaction(block, transaction);
+  logger.info('Transaction added to block');
+  // return it
+  return transaction;
+}
+
 module.exports = async function addNewBlocks(job) {
   let numberOfBlocksAdded = 0;
   let latestBlockNumberInDB = await getLatestBlockNumberInDB();
   const latestBlockNumberInNode = await getLatestBlockNumberFromNode();
-  const latestBlockNumberToAdd =
-    job && job.data && job.data.limit
-      ? latestBlockNumberInDB + Number(job.data.limit)
-      : latestBlockNumberInNode;
+  const latestBlockNumberToAdd = getJobData(job, 'limit')
+    ? latestBlockNumberInDB + Number(getJobData(job, 'limit'))
+    : latestBlockNumberInNode;
 
   logger.info('Block numbers:\n', {
     latestBlockNumberInDB,
@@ -46,18 +93,25 @@ module.exports = async function addNewBlocks(job) {
       logger.info(`Got block #${newBlock.header.blockNumber} from NODE...`);
 
       try {
-        logger.info(`Creating a new block with blockNumber ${newBlock.header.blockNumber}  ...`);
-        await blocksDAL.create({
-          version: newBlock.header.version,
-          parent: newBlock.header.parent,
-          blockNumber: newBlock.header.blockNumber,
-          commitments: newBlock.header.commitments,
-          timestamp: newBlock.header.timestamp,
-          difficulty: newBlock.header.difficulty,
-          nonce1: newBlock.header.nonce[0],
-          nonce2: newBlock.header.nonce[1],
-        });
-        logger.info(`Block #${newBlock.header.blockNumber} created.`);
+        const block = await createBlock(newBlock);
+
+        const skipTransactions = getJobData(job, 'skipTransactions');
+
+        if (!skipTransactions) {
+          // transactions
+          const transactionHashes = Object.keys(newBlock.transactions);
+          const transactionsToAdd = getJobData(job, 'limitTransactions')
+            ? getJobData(job, 'limitTransactions')
+            : transactionHashes.length;
+          for (let i = 0; i < transactionsToAdd; i++) {
+            const hash = transactionHashes[i];
+            const transaction = await addTransactionToBlock(block, newBlock.transactions[hash], hash);
+
+            // add outputs
+
+            // add inputs
+          }
+        }
       } catch (error) {
         logger.error(`Error creating #${newBlock.header.blockNumber}`, error);
         // do not skip a block
