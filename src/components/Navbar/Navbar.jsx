a8c233fd7271229796d21d549ef2eda6502a84a4
Lior Frenkel <hotfur@gmail.com>
2018-07-05 11:02:42 +0200

    Add zen logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import logo from '../../logo_big.png';
+import zenLogo from '../../zen.png';
 import classnames from 'classnames';
 import './Navbar.css';
 
@@ -9,8 +10,9 @@ export default function Navbar(props) {
     <div className={classnames(props.className, 'Navbar')}>
       <nav className="navbar navbar-expand-lg py-1 py-lg-3 px-0">
         <Link className="navbar-brand" to="/">
-          <img src={logo} className="d-inline-block" alt="" />
-          <span>ZEN / BLOCK EXPLORER</span>
+          <img src={logo} className="logo d-inline-block align-middle" alt="" />
+          <img src={zenLogo} className="zen d-inline-block align-middle" alt="" />
+          <div className="subtitle d-inline-block">/ BLOCK EXPLORER</div>
         </Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -25,9 +27,9 @@ export default function Navbar(props) {
               <Link className="nav-link" to="/transactions">Transactions</Link>
             </li> */}
           </ul>
-          <form className="form-inline my-2 my-lg-0">
+          {/* <form className="form-inline my-2 my-lg-0">
             <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-          </form>
+          </form> */}
         </div>
       </nav>
     </div>
