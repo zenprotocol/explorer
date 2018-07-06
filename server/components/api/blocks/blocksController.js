e76e50ddbc213338556156c3c8ea48a4b3c5560d
Lior Frenkel <hotfur@gmail.com>
2018-07-06 18:42:27 +0200

    Sort transaction ins and outs by asset

--- server/components/api/blocks/blocksController.js
+++ server/components/api/blocks/blocksController.js
@@ -6,6 +6,44 @@ const jsonResponse = require('../../../lib/jsonResponse');
 const HttpError = require('../../../lib/HttpError');
 const createQueryObject = require('../../../lib/createQueryObject');
 
+function sortTransactionInsAndOutsByAssets (transaction) {
+  const assets = {};
+  if(transaction.Outputs && transaction.Outputs.length) {
+    transaction.Outputs.forEach((output) => {
+      if(!assets[output.asset]) {
+        assets[output.asset] = {
+          inputs: [],
+          outputs: [],
+        };
+      }
+
+      assets[output.asset].outputs.push(output);
+    });
+  }
+
+  if(transaction.Inputs && transaction.Inputs.length) {
+    transaction.Inputs.forEach((input) => {
+      if (input.Output) {
+        if(!assets[input.Output.asset]) {
+          assets[input.Output.asset] = {
+            inputs: [],
+            outputs: [],
+          };
+        }
+        assets[input.Output.asset].inputs.push(input);
+      }
+    });
+  }
+
+  return Object.keys(assets).map((asset) => {
+    return {
+      asset: asset,
+      inputs: assets[asset].inputs,
+      outputs: assets[asset].outputs,
+    };
+  }).sort((a, b) => b.asset < a.asset);
+}
+
 module.exports = {
   index: async function(req, res) {
     const page = req.query.page || 0;
@@ -27,17 +65,23 @@ module.exports = {
   },
   findByBlockNumber: async function(req, res) {
     const block = await blocksDAL.findByBlockNumber(req.params.id);
-    // make sure the order is right
-    block.Transactions.forEach((transaction) => {
+    const customBlock = block.toJSON();
+    console.log(customBlock);
+    customBlock.Transactions.forEach((transaction) => {
+      // make sure the order is right
       transaction.Outputs.sort((a, b) => {
         return Number(b.index) < Number(a.index);
       });
       transaction.Inputs.sort((a, b) => {
         return Number(b.index) < Number(a.index);
       });
+
+      transaction['assets'] = sortTransactionInsAndOutsByAssets(transaction);
+      delete transaction.Inputs;
+      delete transaction.Outputs;
     });
-    if (block) {
-      res.status(httpStatus.OK).json(jsonResponse.create(httpStatus.OK, block));
+    if (customBlock) {
+      res.status(httpStatus.OK).json(jsonResponse.create(httpStatus.OK, customBlock));
     } else {
       throw new HttpError(httpStatus.NOT_FOUND);
     }
@@ -54,4 +98,5 @@ module.exports = {
     await blocksDAL.delete(req.params.id);
     res.status(httpStatus.OK).json(jsonResponse.create(httpStatus.OK));
   },
+  
 };
