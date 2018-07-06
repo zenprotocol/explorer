8724e78e7717d14af0fab472339b6f96077e40a2
Lior Frenkel <hotfur@gmail.com>
2018-07-06 18:41:09 +0200

    Find by transaction id

--- server/components/api/outputs/outputsDAL.js
+++ server/components/api/outputs/outputsDAL.js
@@ -4,4 +4,15 @@ const dal = require('../../../lib/dal');
 
 const outputsDAL = dal.createDAL('Output');
 
+outputsDAL.findByTransaction = function(transactionId) {
+  return this.findAll({
+    where: {
+      TransactionId: transactionId
+    },
+    order: [
+      ['asset']
+    ]
+  });
+};
+
 module.exports = outputsDAL;
