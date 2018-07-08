040e2bebf106e84dca407749780571841d8c53d2
Lior Frenkel <hotfur@gmail.com>
2018-07-08 10:30:18 +0200

    Add switch case indent

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     ecmaVersion: 8,
   },
   rules: {
-    indent: ['error', 2],
+    indent: [2, 2, {"SwitchCase": 1}],
     'linebreak-style': ['off', 'windows'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
