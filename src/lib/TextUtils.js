416567ccb89528293af5d337938fb28e7314d49c
Lior Frenkel <hotfur@gmail.com>
2018-07-12 18:20:33 +0200

    Use UTC string to display the date

--- src/lib/TextUtils.js
+++ src/lib/TextUtils.js
@@ -1,8 +1,10 @@
 export default {
   getDateString(date) {
     if (date && typeof date === 'object') {
-      const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-      return `${date.getUTCDate()} ${months[date.getUTCMonth()]} ${date.getUTCFullYear()} ${date.getUTCHours()}:${date.getUTCMinutes()}`;
+      // const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+      // return `${date.getUTCDate()} ${months[date.getUTCMonth()]} ${date.getUTCFullYear()} ${date.getUTCHours()}:${date.getUTCMinutes()}`;
+      const dateUTC = date.toUTCString();
+      return dateUTC.substring(dateUTC.indexOf(',') + 1, dateUTC.lastIndexOf(':')).trim();
     }
 
     return '';
