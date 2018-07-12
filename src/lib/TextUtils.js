76e1c1dc26170e7a48f5828e67882b989373f2ca
Lior Frenkel <hotfur@gmail.com>
2018-07-12 17:04:37 +0200

    Fix date

--- src/lib/TextUtils.js
+++ src/lib/TextUtils.js
@@ -2,7 +2,7 @@ export default {
   getDateString(date) {
     if (date && typeof date === 'object') {
       const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-      return `${date.getUTCDate()} ${months[date.getUTCMonth() - 1]} ${date.getUTCFullYear()} ${date.getUTCHours()}:${date.getUTCMinutes()}`;
+      return `${date.getUTCDate()} ${months[date.getUTCMonth()]} ${date.getUTCFullYear()} ${date.getUTCHours()}:${date.getUTCMinutes()}`;
     }
 
     return '';
