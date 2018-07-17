73deff60d64cfc17cad936c85507fb17f80fabbf
Lior Frenkel <hotfur@gmail.com>
2018-07-17 10:27:12 +0200

    Update logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
 import logo from '../../logo.png';
+import logoAll from '../../logo_all.png';
 
 export default function Logo(props) {
   return (
     <Link className="navbar-brand text-primary" to="/">
-      <img src={logo} className="logo d-inline-block align-middle" alt="" />
       {!props.hideSubtitle? (
-        <div className="subtitle d-none d-sm-inline-block">/ BLOCK EXPLORER</div>
-      ): null}
+        <img src={logoAll} className="logo d-inline-block align-middle" alt="" />
+      ): (
+        <img src={logo} className="logo d-inline-block align-middle" alt="" />
+      )}
     </Link>
   );
 }
+
+Logo.propTypes = {
+  hideSubtitle: PropTypes.bool,
+};
