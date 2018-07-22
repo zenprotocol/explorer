19103f74003ee55a70831be8b3609a5388296ae6
Lior Frenkel <hotfur@gmail.com>
2018-07-22 15:34:40 +0200

    Custom format

--- worker/lib/logger.js
+++ worker/lib/logger.js
@@ -2,27 +2,30 @@
 
 const fs = require('fs');
 const path = require('path');
-const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, json } = format;
 const Config = require('../../server/config/Config');
 
 var logsDir = path.join(__dirname, '../../logs');
-if (!fs.existsSync(logsDir)){
+if (!fs.existsSync(logsDir)) {
   fs.mkdirSync(logsDir);
 }
 
-const logger = winston.createLogger({
+const logger = createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: combine(label(), timestamp(), json()),
   transports: [
-    new winston.transports.File({ filename: path.join(logsDir, 'error.log'), level: 'error' }),
-    new winston.transports.File({ filename: path.join(logsDir, 'combined.log')})
-  ]
+    new transports.File({ filename: path.join(logsDir, 'error.log'), level: 'error' }),
+    new transports.File({ filename: path.join(logsDir, 'combined.log') }),
+  ],
 });
 
 if (process.env.NODE_ENV === 'development' || Config.get('SHOW_CONSOLE_LOGS')) {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple()
-  }));
+  logger.add(
+    new transports.Console({
+      format: format.simple(),
+    })
+  );
 }
 
 module.exports = logger;
