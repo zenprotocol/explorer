f06f71729157b00af4272cbec9a36ebc9c82a289
Lior Frenkel <hotfur@gmail.com>
2018-08-16 07:42:47 +0200

    Add total kalapa stat

--- server/components/api/stats/statsController.js
+++ server/components/api/stats/statsController.js
@@ -5,22 +5,27 @@ const jsonResponse = require('../../../lib/jsonResponse');
 const HttpError = require('../../../lib/HttpError');
 const blocksDAL = require('../blocks/blocksDAL');
 
-const STATS = ['totalzp'];
+const STATS = ['totalzp', 'total_kalapa'];
 
 const StatsFunctions = {
   totalZp: async function() {
     const blocksCount = await blocksDAL.count();
-    return (20000000 + (blocksCount - 1) * 50) * 100000000;
+    return 20000000 + (blocksCount - 1) * 50;
+  },
+  totalKalapa: async function() {
+    return (await this.totalZp()) * 100000000;
   },
 };
 
 module.exports = {
   index: async function(req, res) {
     const totalZP = await StatsFunctions.totalZp();
+    const totalKalapa = await StatsFunctions.totalKalapa();
 
     res.status(httpStatus.OK).json(
       jsonResponse.create(httpStatus.OK, {
-        totalZP
+        totalZP,
+        totalKalapa,
       })
     );
   },
@@ -32,9 +37,12 @@ module.exports = {
     }
 
     let stat = null;
-    switch(name) {
+    switch (name) {
       case 'totalzp':
         stat = await StatsFunctions.totalZp();
+        break;
+      case 'total_kalapa':
+        stat = await StatsFunctions.totalKalapa();
         break;
     }
 
