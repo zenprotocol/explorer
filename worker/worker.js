d40d94e8a16a804601222ea479cd553f181e0b6a
Lior Frenkel <hotfur@gmail.com>
2018-08-20 07:05:36 +0200

    Increase amount of blocks per job

--- worker/worker.js
+++ worker/worker.js
@@ -40,7 +40,7 @@ addBlocksQueue.on('cleaned', function(jobs, type) {
 });
 
 // schedule ---
-addBlocksQueue.add({limitBlocks: 50}, { repeat: { cron: '* * * * *' } });
+addBlocksQueue.add({limitBlocks: 200}, { repeat: { cron: '* * * * *' } });
 
 setInterval(() => {
   addBlocksQueue.clean(Config.get('queues:addBlocks:cleanAfter') * 1000, 'completed');
