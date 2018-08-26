14a563ed964ac02d2eccecbe4a802b8c503b9078
Lior Frenkel <hotfur@gmail.com>
2018-08-26 17:09:17 +0200

    Add search rules

--- server/components/api/search/searchController.js
+++ server/components/api/search/searchController.js
@@ -11,6 +11,29 @@ function isSearchStringValid(searchString) {
   return searchString && searchString.length >= 3 && searchString !== 'zen' && searchString !== 'zen1';
 }
 
+function getSearchPromises(search, limit) {
+  const searchFor = {
+    blocks: true,
+    transactions: true,
+    addresses: true,
+  };
+
+  if(search.startsWith('zen1')) {
+    searchFor.blocks = false;
+    searchFor.transactions = false;
+  }
+  // else if (!isNaN(Number(search)) && search.length < 7) {
+  //   searchFor.addresses = false;
+  //   searchFor.transactions = false;
+  // }
+
+  return [
+    searchFor.blocks ? blocksDAL.search(search, limit) : Promise.resolve([0, []]),
+    searchFor.transactions ? transactionsDAL.search(search, limit) : Promise.resolve([0, []]),
+    searchFor.addresses ? addressesDAL.search(search, limit) : Promise.resolve([0, []]),
+  ];
+}
+
 module.exports = {
   index: async function(req, res) {
     let search = req.params.search;
@@ -19,12 +42,8 @@ module.exports = {
     }
 
     search = search.trim().toLowerCase();
-
-    const [blocks, transactions, addresses] = await Promise.all([
-      blocksDAL.search(search),
-      transactionsDAL.search(search),
-      addressesDAL.search(search),
-    ]);
+    const searchResultsLimit = 5;
+    const [blocks, transactions, addresses] = await Promise.all(getSearchPromises(search, searchResultsLimit));
 
     res.status(httpStatus.OK).json(
       jsonResponse.create(httpStatus.OK, {
