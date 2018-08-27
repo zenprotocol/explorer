661d51cef891492f630903e295ec4caae9d9db88
Elan Perach <elanperach@gmail.com>
2018-08-27 19:24:04 +0300

    remove blocks link in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,12 +16,14 @@ export default function Navbar(props) {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <NavLink className="nav-link" to="/blocks">Blocks</NavLink>
-            </li>
-            {/* <li className="nav-item">
-              <NavLink className="nav-link" to="/tx">Transactions</NavLink>
-            </li> */}
+            {/*
+              <li className="nav-item active">
+                <NavLink className="nav-link" to="/blocks">Blocks</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/tx">Transactions</NavLink>
+              </li>
+            */}
           </ul>
           <SearchBar />
         </div>
