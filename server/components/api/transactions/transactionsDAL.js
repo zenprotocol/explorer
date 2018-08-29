cb3db479a53971533cf5bfd18c09acda48c466a6
Lior Frenkel <hotfur@gmail.com>
2018-08-29 21:29:33 +0200

    Improve sql for assetsByBlocks

--- server/components/api/transactions/transactionsDAL.js
+++ server/components/api/transactions/transactionsDAL.js
@@ -161,7 +161,7 @@ transactionsDAL.findAllAssetsByBlock = async function(
 
     ON "OutputAsset"."TransactionId" = "InputAsset"."TransactionId"
       AND "OutputAsset"."asset" = "InputAsset"."asset"
-    INNER JOIN "Transactions" AS "Transaction" ON "OutputAsset"."TransactionId" = "Transaction"."id"
+    INNER JOIN "Transactions" AS "Transaction" ON "OutputAsset"."TransactionId" = "Transaction"."id" OR "InputAsset"."TransactionId" = "Transaction"."id"
     INNER JOIN "Blocks" AS "Block" ON "Transaction"."BlockId" = "Block"."id"
   WHERE "Block"."${blockProp}" = :hashOrBlockNumber
   LIMIT :limit OFFSET :offset`;
@@ -240,8 +240,7 @@ transactionsDAL.countAssetsByBlock = async function(hashOrBlockNumber) {
   const blockProp = isHash(hashOrBlockNumber) ? 'hash' : 'blockNumber';
   const sequelize = transactionsDAL.db.sequelize;
   const sql = `
-  SELECT
-    COUNT("Transaction"."id")
+  SELECT COUNT("OutputAsset"."TransactionId")
   FROM
     (SELECT SUM("Output"."amount") AS "outputSum",
       "Output"."asset",
@@ -263,10 +262,7 @@ transactionsDAL.countAssetsByBlock = async function(hashOrBlockNumber) {
     GROUP BY "Input"."TransactionId", "Output"."asset") AS "InputAsset"
 
     ON "OutputAsset"."TransactionId" = "InputAsset"."TransactionId"
-      AND "OutputAsset"."asset" = "InputAsset"."asset"
-    INNER JOIN "Transactions" AS "Transaction" ON "OutputAsset"."TransactionId" = "Transaction"."id"
-    INNER JOIN "Blocks" AS "Block" ON "Transaction"."BlockId" = "Block"."id"
-  WHERE "Block"."${blockProp}" = :hashOrBlockNumber`;
+      AND "OutputAsset"."asset" = "InputAsset"."asset"`;
 
   return sequelize
     .query(sql, {
