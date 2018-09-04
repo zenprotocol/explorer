afb2ac6da5cb342bf4467fa3c3d3ee399b82cdd3
Lior Frenkel <hotfur@gmail.com>
2018-09-04 22:53:23 +0200

    Fine tuning styles

--- src/routes/info/Info.jsx
+++ src/routes/info/Info.jsx
@@ -11,11 +11,11 @@ function ContentBox(props) {
   return (
     <div className="col border border-dark">
       <div className={classNames('content-box d-flex align-items-center flex-wrap', props.className)}>
-        <div className="d-flex align-items-center">
+        <div className="content-wrapper d-flex align-items-center">
           <div className="icon text-secondary d-flex align-items-center justify-content-center">
             <i className={props.iconClass} />
           </div>
-          <div className="content mr-4">
+          <div className="content">
             <div className="title text-secondary">{props.title}</div>
             <div className="value text-white text-monospace">{props.content}</div>
           </div>
