46659b603dcdf8d6e24f1c68dadc9be29c58ddac
Lior Frenkel <hotfur@gmail.com>
2018-09-21 12:05:26 +0200

    Scroll to top on location change

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App.js';
+import ScrollToTop from './components/scroll/ScrollToTop.jsx';
 import './polyfills';
 import './style/index.css';
 import './index.css';
-import App from './App.js';
 // import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
   </BrowserRouter>,
   document.getElementById('root')
 );
