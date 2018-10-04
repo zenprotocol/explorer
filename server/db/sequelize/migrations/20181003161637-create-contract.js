509285d156755ccf0f704624948c36e2bcd37053
Lior Frenkel <hotfur@gmail.com>
2018-10-04 15:29:32 +0200

    Change code to a text field

--- server/db/sequelize/migrations/20181003161637-create-contract.js
+++ server/db/sequelize/migrations/20181003161637-create-contract.js
@@ -14,7 +14,7 @@ module.exports = {
         allowNull: false,
       },
       code: {
-        type: Sequelize.STRING,
+        type: Sequelize.TEXT,
         allowNull: false,
       },
       expiryBlock: {
