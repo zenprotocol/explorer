4c2771ab0a2663db9f2626251bfa7e26dfacc3fe
Lior Frenkel <hotfur@gmail.com>
2018-10-04 11:52:41 +0200

    Add contracts table

---
+++ server/db/sequelize/migrations/20181003161637-create-contract.js
@@ -0,0 +1,36 @@
+'use strict';
+const TABLE_NAME = 'Contracts';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable(TABLE_NAME, {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING,
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      code: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      expiryBlock: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface) => {
+    return queryInterface.dropTable(TABLE_NAME);
+  },
+};
