41a08e2e3fdfa109b2a9c2f5dd2e280ee5a37672
Lior Frenkel <hotfur@gmail.com>
2018-10-07 15:03:28 +0200

    Add tagline and style

--- src/components/PageTitle/PageTitle.jsx
+++ src/components/PageTitle/PageTitle.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './PageTitle.css';
 
-export default function PageTitle({ title, subtitle }) {
+export default function PageTitle({ title, subtitle, tagline }) {
   return (
-    <div className="PageTitle row">
-      <div className="col-sm">
-        <h1 className="d-block d-sm-inline-block text-white mb-3 mb-lg-5">
-          {title}
-          <div className="subtitle break-word">{subtitle}</div>
-        </h1>
-      </div>
+    <div className="PageTitle">
+      {tagline && <div className="font-size-md mb-1 mb-lg-2">{tagline}</div>}
+      <h1 className="d-block d-sm-inline-block text-white mb-3 mb-lg-5">
+        {title}
+        {subtitle && <div className="subtitle break-word">{subtitle}</div>}
+      </h1>
     </div>
   );
 }
@@ -17,4 +17,5 @@ export default function PageTitle({ title, subtitle }) {
 PageTitle.propTypes = {
   title: PropTypes.string,
   subtitle: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
+  tagline: PropTypes.string,
 };
