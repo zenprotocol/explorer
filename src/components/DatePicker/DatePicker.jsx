6fa603834081e546148a1f23808d1f6690b46b6b
Lior Frenkel <hotfur@gmail.com>
2018-10-12 16:29:29 +0200

    Add react-day-picker

---
+++ src/components/DatePicker/DatePicker.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import DayPicker from 'react-day-picker/DayPickerInput';
+import './DatePicker.css';
+
+export default class DatePicker extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onDateChange = this.onDateChange.bind(this);
+  }
+
+  onDateChange(selectedDay, modifiers, dayPickerInput) {
+    const input = dayPickerInput.getInput();
+    this.props.onDateChange({
+      value: input.value.trim(),
+      isEmpty: !input.value.trim(),
+      isValid: typeof selectedDay !== 'undefined',
+    });
+  }
+
+  render() {
+    const { value } = this.props;
+    return (
+      <div className="DatePicker">
+        <DayPicker onDayChange={this.onDateChange} value={value} formatDate={formatDate}/>
+      </div>
+    );
+  }
+}
+
+function formatDate(d) {
+  return new Date(d).toISOString().split('T')[0];
+}
+
+DatePicker.propTypes = {
+  onDateChange: PropTypes.func.isRequired,
+  value: PropTypes.string,
+};
