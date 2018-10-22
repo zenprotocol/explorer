cfc8001c28f74d11b4f07ce076bcb6eaf718dfb2
Lior Frenkel <hotfur@gmail.com>
2018-10-22 14:59:26 +0200

    Find all contract assets outstanding

--- server/components/api/contracts/contractsController.js
+++ server/components/api/contracts/contractsController.js
@@ -4,6 +4,7 @@ const httpStatus = require('http-status');
 const contractsDAL = require('./contractsDAL');
 const jsonResponse = require('../../../lib/jsonResponse');
 const HttpError = require('../../../lib/HttpError');
+const createQueryObject = require('../../../lib/createQueryObject');
 
 module.exports = {
   index: async function(req, res) {
@@ -21,6 +22,23 @@ module.exports = {
     const contract = await contractsDAL.findByAddress(req.params.address);
     if (contract) {
       res.status(httpStatus.OK).json(jsonResponse.create(httpStatus.OK, contract));
+    } else {
+      throw new HttpError(httpStatus.NOT_FOUND);
+    }
+  },
+  assets: async function(req, res) {
+    if(!req.params.address) {
+      throw new HttpError(httpStatus.BAD_REQUEST);
+    }
+
+    const contract = await contractsDAL.findByAddress(req.params.address);
+    if (contract) {
+      const page = Number(req.query.page) || 0;
+      const pageSize = Number(req.query.pageSize) || 10;
+
+      const query = createQueryObject({ page, pageSize, sorted: [] });
+      const assets = await contractsDAL.findAllOutstandingAssets(contract.id, query);
+      res.status(httpStatus.OK).json(jsonResponse.create(httpStatus.OK, assets));
     } else {
       throw new HttpError(httpStatus.NOT_FOUND);
     }
--- server/components/api/contracts/contractsDAL.js
+++ server/components/api/contracts/contractsDAL.js
@@ -31,6 +31,50 @@ contractsDAL.findAllExpired = function() {
   });
 };
 
+contractsDAL.findAllOutstandingAssets = function(id, { limit = 10, offset = 0 } = {}) {
+  const sequelize = contractsDAL.db.sequelize;
+  const sql = `
+  SELECT
+    COALESCE("Issued"."asset", "Destroyed"."asset") AS "asset",
+    "Issued"."sum" AS "issued",
+    "Destroyed"."sum" AS "destroyed",
+    COALESCE("Issued"."sum", 0) -  COALESCE("Destroyed"."sum", 0) AS "outstanding",
+    "Keyholders"."total" AS "keyholders"
+  FROM
+    (SELECT asset, SUM("amount") AS sum
+    FROM "Inputs"
+    WHERE "Inputs"."asset" LIKE :assetSearch
+      AND "Inputs"."isMint" = 'true'
+    GROUP BY "Inputs"."asset") AS "Issued"
+    LEFT JOIN
+    (SELECT asset, SUM("amount") AS sum
+    FROM "Outputs"
+    WHERE "Outputs"."asset" LIKE :assetSearch
+      AND "Outputs"."lockType" = 'Destroy'
+    GROUP BY "Outputs"."asset") AS "Destroyed"
+    ON "Issued"."asset" = "Destroyed"."asset" 
+    LEFT JOIN
+    (SELECT COUNT("UniqueAddresses"."address") AS "total", "UniqueAddresses"."asset"
+    FROM
+      (SELECT "address", "asset"
+      FROM "Outputs"
+      WHERE "Outputs"."asset" LIKE :assetSearch
+      GROUP BY "Outputs"."asset", "Outputs"."address") AS "UniqueAddresses"
+    GROUP BY "UniqueAddresses"."asset") AS "Keyholders"
+    ON "Issued"."asset" = "Keyholders"."asset"
+    LIMIT :limit OFFSET :offset
+  `;
+
+  return sequelize.query(sql, {
+    replacements: {
+      assetSearch: `${id}%`,
+      limit,
+      offset,
+    },
+    type: sequelize.QueryTypes.SELECT,
+  });
+};
+
 contractsDAL.setExpired = function(ids = []) {
   if (!ids.length) {
     return Promise.resolve();
