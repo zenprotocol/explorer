d353a4b27c03ea8d4cd78d33bf4f873a0e68f3d3
Lior Frenkel <hotfur@gmail.com>
2018-10-28 17:11:34 +0100

    Use the contracts worker just to update the expiryBlock

---
+++ worker/jobs/contracts/ActiveContractsUpdater.test.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const test = require('blue-tape');
+const truncate = require('../../../common/test/truncate');
+const NetworkHelper = require('../../lib/NetworkHelper');
+const mock = require('./test/mock');
+const contractsDAL = require('../../../server/components/api/contracts/contractsDAL');
+const ActiveContractsUpdater = require('./ActiveContractsUpdater');
+const activeContractsMock = require('./test/data/activeContracts.json');
+const convertContractToDbContract = require('./test/convertContractToDbContract');
+
+function getMockedActiveContractsUpdater() {
+  const networkHelper = new NetworkHelper();
+  mock.mockNetworkHelper(networkHelper);
+  return new ActiveContractsUpdater(networkHelper);
+}
+
+async function addAllMocksToDb(expiryBlock = null) {
+  for (let i = 0; i < activeContractsMock.length; i++) {
+    const nodeContract = activeContractsMock[i];
+    const contractValues = convertContractToDbContract(nodeContract);
+    contractValues.expiryBlock = expiryBlock;
+    await contractsDAL.create(contractValues);
+  }
+}
+
+test.onFinish(() => {
+  contractsDAL.db.sequelize.close();
+});
+
+test('ContractsAdder.doJob()', async function(t) {
+  await givenNoContractsInDb(t);
+  await givenAllContractsInDbWithNull(t);
+  await givenContractInDbButNotInACS(t);
+  await extendContracts(t);
+});
+
+async function givenNoContractsInDb(t) {
+  await truncate();
+  const contractsAdder = getMockedActiveContractsUpdater();
+
+  t.equal(await contractsAdder.doJob(), 0, 'Given no contracts in db: Should not update contracts');
+}
+
+async function givenAllContractsInDbWithNull(t) {
+  await truncate();
+  const contractsAdder = getMockedActiveContractsUpdater();
+  await addAllMocksToDb();
+  t.equal(
+    await contractsAdder.doJob(),
+    activeContractsMock.length,
+    'Given contracts in db with null expire: Should update all contracts'
+  );
+}
+
+async function givenContractInDbButNotInACS(t) {
+  await truncate();
+  const contractsAdder = getMockedActiveContractsUpdater();
+  await contractsDAL.create({
+    id: '1234',
+    address: '5678',
+    expiryBlock: 40000,
+    code: 'nothing to test here',
+  });
+
+  await contractsAdder.doJob();
+  const contract = await contractsDAL.findById('1234');
+  t.equal(
+    contract.expiryBlock,
+    null,
+    'Given a contract in db but not in ACS: expiryBlock should be null'
+  );
+}
+
+async function extendContracts(t) {
+  await truncate();
+  const contractsAdder = getMockedActiveContractsUpdater();
+  await addAllMocksToDb(100);
+  t.equal(
+    await contractsAdder.doJob(),
+    activeContractsMock.length,
+    'Given all contracts with smaller expire: should extend all contracts'
+  );
+  const contract = await contractsDAL.findById(activeContractsMock[0].contractId);
+  t.equal(
+    contract.expiryBlock,
+    activeContractsMock[0].expire,
+    'Given a contract with a smaller expire: should extent the contract'
+  );
+}
