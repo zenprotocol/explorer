abd3afe3463718ea1dbd597dc164181b274036a6
Lior Frenkel <hotfur@gmail.com>
2018-11-14 16:12:50 +0100

    Get destroyed and outstanding for ZP

--- server/components/api/assets/assetsDAL.js
+++ server/components/api/assets/assetsDAL.js
@@ -4,9 +4,11 @@ const tags = require('common-tags');
 const sequelize = require('../../../db/sequelize/models').sequelize;
 const dal = require('../../../lib/dal');
 const statsDAL = require('../stats/statsDAL');
+const outputsDAL = require('../outputs/outputsDAL');
 const sqlQueries = require('../../../lib/sqlQueries');
 
 const assetsDAL = dal.createDAL('');
+const Op = assetsDAL.db.sequelize.Op;
 
 assetsDAL.findOutstanding = function(asset) {
   const sql = tags.oneLine`
@@ -49,11 +51,20 @@ assetsDAL.findOutstanding = function(asset) {
 };
 
 assetsDAL.findZP = function() {
-  return Promise.all([statsDAL.totalZp(), statsDAL.distributionMapCount('00')]).then(
-    ([issued, keyholders]) => {
+  return Promise.all([statsDAL.totalZp(), outputsDAL.sum('amount', {
+    where: {
+      [Op.and]: {
+        asset: '00',
+        lockType: 'Destroy',
+      }
+    }
+  }), statsDAL.distributionMapCount('00')]).then(
+    ([issued, destroyed, keyholders]) => {
       return {
         asset: '00',
         issued: Math.floor(issued * 100000000),
+        destroyed: destroyed || 0,
+        outstanding: Math.floor(issued * 100000000) - (destroyed || 0),
         keyholders,
       };
     }
