b87c93e27c1d862873787c171e4b96536cdd4703
Lior Frenkel <hotfur@gmail.com>
2018-12-19 10:53:18 +0100

    Display difficulty in decimal units

--- src/lib/BlockUtils.js
+++ src/lib/BlockUtils.js
@@ -1,8 +1,17 @@
 export default {
   formatDifficulty(difficulty) {
-    if(difficulty) {
-      return `0x${Number(difficulty).toString(16)}`;
+    if (difficulty) {
+      return conversions.decimal(difficulty);
     }
     return '';
-  }
+  },
+};
+
+const conversions = {
+  decimal(difficulty) {
+    return difficulty;
+  },
+  hexadecimal(difficulty) {
+    return `0x${Number(difficulty).toString(16)}`;
+  },
 };
