b6b826c8d830b5ede2f3444ea86926fe7d4c9540
Lior Frenkel <frenkeldev@gmail.com>
2019-01-16 19:14:59 +0100

    Add a download contract button

--- src/pages/contract/components/tabs/CodeTab.jsx
+++ src/pages/contract/components/tabs/CodeTab.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react';
 import Highlight from 'react-highlight';
+import fileDownload from 'js-file-download';
 import { TabPanel } from '../../../../components/tabs';
 import Loading from '../../../../components/Loading';
+import Button from '../../../../components/buttons/Button';
 
 export default inject('rootStore')(observer(function CodeTab(props) {
   const { contractStore } = props.rootStore;
@@ -12,6 +14,11 @@ export default inject('rootStore')(observer(function CodeTab(props) {
   return (
     <TabPanel>
       <Highlight className="fsharp">{contractStore.contract.code}</Highlight>
+      <Button size="sm" onClick={() => download(contractStore.contract)}>Download contract</Button>
     </TabPanel>
   );
 }));
+
+function download(contract) {
+  fileDownload(contract.code, `${contract.id}.fst`);
+}
