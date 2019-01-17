9833269802f68aa4ea65dc7cdf2e227cf5672d98
Lior Frenkel <frenkeldev@gmail.com>
2019-01-17 14:43:55 +0100

    Change placement of download button

--- src/pages/contract/components/tabs/CodeTab.jsx
+++ src/pages/contract/components/tabs/CodeTab.jsx
@@ -6,18 +6,27 @@ import { TabPanel } from '../../../../components/tabs';
 import Loading from '../../../../components/Loading';
 import Button from '../../../../components/buttons/Button';
 
-export default inject('rootStore')(observer(function CodeTab(props) {
-  const { contractStore } = props.rootStore;
-  if (contractStore.loading.contract) {
-    return <Loading />;
-  }
-  return (
-    <TabPanel>
-      <Highlight className="fsharp">{contractStore.contract.code}</Highlight>
-      <Button size="sm" onClick={() => download(contractStore.contract)}>Download contract</Button>
-    </TabPanel>
-  );
-}));
+export default inject('rootStore')(
+  observer(function CodeTab(props) {
+    const { contractStore } = props.rootStore;
+    if (contractStore.loading.contract) {
+      return <Loading />;
+    }
+    return (
+      <TabPanel>
+        <Button
+          className="float-right mb-4"
+          size="sm"
+          onClick={() => download(contractStore.contract)}
+        >
+          Download contract
+        </Button>
+        <div className="clearfix" />
+        <Highlight className="fsharp">{contractStore.contract.code}</Highlight>
+      </TabPanel>
+    );
+  })
+);
 
 function download(contract) {
   fileDownload(contract.code, `${contract.id}.fst`);
