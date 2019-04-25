5c022edbb0f1457bcc2c38fbb82e41920581b22f
Lior Frenkel <frenkeldev@gmail.com>
2019-04-25 15:24:32 +0200

    Change txid to command id

--- server/db/sequelize/models/repovote.js
+++ server/db/sequelize/models/repovote.js
@@ -3,7 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   const RepoVote = sequelize.define(
     'RepoVote',
     {
-      TransactionId: DataTypes.BIGINT,
+      CommandId: DataTypes.BIGINT,
       interval: DataTypes.INTEGER,
       commitId: DataTypes.STRING,
       address: DataTypes.STRING,
