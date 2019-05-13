68d6aa5683a73eef1e7a66d9befac06be8aa9a73
Lior Frenkel <frenkeldev@gmail.com>
2019-05-13 14:54:32 +0200

    Link to latest block

--- src/components/LatestBlockInfo/LatestBlockInfo.jsx
+++ src/components/LatestBlockInfo/LatestBlockInfo.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import TextUtils from '../../lib/TextUtils';
 
 function LatestBlockInfo() {
   const { blockStore } = this.props.rootStore;
   const { blockNumber, timestamp } = blockStore.latestBlock;
-  if(blockStore.loading.latestBlock || !Object.keys(blockStore.latestBlock).length) {
+  if (blockStore.loading.latestBlock || !Object.keys(blockStore.latestBlock).length) {
     return null;
   }
-  
+
   return (
     <div className="LatestBlockInfo">
-      <span className="latest">Latest block: {blockNumber}</span>{', '}
+      <span className="latest">Latest block: </span>
+      <Link to={`/blocks/${blockNumber}`}>{blockNumber}</Link>
+      {', '}
       <span className="timezone">{TextUtils.getDateStringFromTimestamp(timestamp)}</span>
     </div>
   );
