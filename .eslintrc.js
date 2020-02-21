e45372617aa4a77a074c16951e8cdf6f8c1fbb9a
Lior Frenkel <lior@zenprotocol.com>
2020-02-21 11:33:42 +0100

    chore: eslint ignore indent

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
     },
   },
   rules: {
-    indent: ['error', 2, { SwitchCase: 1, flatTernaryExpressions: true }],
+    indent: 0,
     'linebreak-style': ['off', 'windows'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
