61442e39eb2f5add13898b7f07ebf45c391a8451
Lior Frenkel <lior@zenprotocol.com>
2020-02-26 16:21:37 +0100

    feat: remove trailing zeros in decimal part

--- src/lib/AssetUtils.js
+++ src/lib/AssetUtils.js
@@ -18,9 +18,7 @@ export default {
     const amountDivided = new Decimal(this.getAmountDivided(asset, amount));
 
     if (this.isZP(asset)) {
-      const parsedAmount = amountDivided.floor().equals(amountDivided)
-        ? amountDivided.toString()
-        : amountDivided.toFixed(8);
+      const parsedAmount = amountDivided.toFixed(Math.min(8, amountDivided.decimalPlaces()));
       return `${TextUtils.formatNumber(parsedAmount)} ${this.getAssetNameFromCode(asset)}`;
     }
     return String(TextUtils.formatNumber(amount));
