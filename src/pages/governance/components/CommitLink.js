e4fe787de2b0eee39eedfd11bbc8a59d39c35126
Lior Frenkel <lior@zenprotocol.com>
2020-03-29 15:06:02 +0200

    feat(governance): allow vote for default commit

--- src/pages/governance/components/CommitLink.js
+++ src/pages/governance/components/CommitLink.js
@@ -2,15 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import HashLink from '../../../components/HashLink';
 
+const DEFAULT_COMMIT_ID = '0000000000000000000000000000000000000000';
+
 export default function CommitLink({ commitId, ...props }) {
-  return (
-    <HashLink
-      url={`https://gitlab.com/zenprotocol/zenprotocol/commit/${commitId}`}
-      hash={commitId}
-      external
-      {...props}
-    />
-  );
+  const url =
+    commitId === DEFAULT_COMMIT_ID
+      ? 'https://gitlab.com/zenprotocol/zenprotocol'
+      : `https://gitlab.com/zenprotocol/zenprotocol/commit/${commitId}`;
+  return <HashLink url={url} hash={commitId} external {...props} />;
 }
 CommitLink.propTypes = {
   commitId: PropTypes.string,
