cce84f06f595999a08fa4f2141a729bf076413af
Lior Frenkel <lior@zenprotocol.com>
2020-10-12 20:17:58 +0300

    fix: revert asset and address on reorg

--- worker/jobs/blocks/ReorgProcessor.js
+++ worker/jobs/blocks/ReorgProcessor.js
@@ -1,24 +1,39 @@
 'use strict';
 
 const EventEmitter = require('events');
+const { Decimal } = require('decimal.js');
 const logger = require('../../lib/logger')('reorg');
 const blocksDAL = require('../../../server/components/api/blocks/blocksDAL');
+const txsDAL = require('../../../server/components/api/txs/txsDAL');
+const contractsDAL = require('../../../server/components/api/contracts/contractsDAL');
+const addressesDAL = require('../../../server/components/api/addresses/addressesDAL');
+const assetsDAL = require('../../../server/components/api/assets/assetsDAL');
+const inputsDAL = require('../../../server/components/api/inputs/inputsDAL');
+const outputsDAL = require('../../../server/components/api/outputs/outputsDAL');
 const db = require('../../../server/db/sequelize/models');
+const calcRewardByHeight = require('../../../server/lib/calcRewardByHeight');
 const getJobData = require('../../lib/getJobData');
 const QueueError = require('../../lib/QueueError');
+const calcAddressesAssets = require('./calcAddressesAssetsPerTx');
+
 const Op = db.Sequelize.Op;
 
 const MAX_ALLOWED_BLOCKS_TO_DELETE = 500;
 
 class ReorgProcessor extends EventEmitter {
-  constructor(networkHelper) {
+  constructor(networkHelper, genesisTotalZp) {
     super();
     this.networkHelper = networkHelper;
+    this.genesisTotalZp = genesisTotalZp;
+    this.dbTransaction = null;
   }
 
   async doJob(job) {
-    this.on('fork-found', blockNumber => logger.info(`Fork found at block number ${blockNumber}`));
+    this.on('fork-found', (blockNumber) =>
+      logger.info(`Fork found at block number ${blockNumber}`)
+    );
     try {
+      this.dbTransaction = await db.sequelize.transaction();
       const searchAll = getJobData(job, 'all') === true; // do not search all by default
       const preventDelete = getJobData(job, 'delete') === false; // delete by default
       let deleted = 0;
@@ -34,6 +49,29 @@ class ReorgProcessor extends EventEmitter {
           logger.info('delete flag is marked false, will not delete blocks.');
         } else {
           const lowestFork = forks[forks.length - 1];
+          logger.info(`Undoing Address, Asset and Contract changes in blockNumber > ${lowestFork}`);
+          // get the txs, last to first
+          const txs = await txsDAL.findAll({
+            where: {
+              blockNumber: {
+                [Op.gt]: lowestFork,
+              },
+            },
+            order: [
+              ['blockNumber', 'DESC'],
+              ['index', 'DESC'],
+            ],
+          });
+
+          for (let i = 0; i < txs.length; i++) {
+            const tx = txs[i];
+            const [inputs, outputs] = await Promise.all([
+              inputsDAL.findAll({ where: { txId: tx.id } }),
+              outputsDAL.findAll({ where: { txId: tx.id } }),
+            ]);
+            await this.calcAddressAssetsPerTx({ inputs, outputs, tx });
+          }
+
           logger.info(`Deleting all blocks with blockNumber > ${lowestFork}`);
           deleted = await blocksDAL.bulkDelete({
             where: {
@@ -41,15 +79,22 @@ class ReorgProcessor extends EventEmitter {
                 [Op.gt]: lowestFork,
               },
             },
+            transaction: this.dbTransaction,
           });
         }
       } else {
         logger.info('Did not find a fork');
       }
 
+      await this.dbTransaction.commit();
+
       return { forks, deleted };
     } catch (error) {
       logger.error(`An Error has occurred when processing a reorg: ${error.message}`);
+      if (this.dbTransaction) {
+        logger.info('Rollback the database transaction');
+        await this.dbTransaction.rollback();
+      }
       throw new QueueError(error);
     }
   }
@@ -78,7 +123,7 @@ class ReorgProcessor extends EventEmitter {
             this.emit('fork-found', blockNumber);
             forks.push(blockNumber);
             foundDifference = false;
-            if(!searchAll) {
+            if (!searchAll) {
               break;
             }
           }
@@ -89,11 +134,149 @@ class ReorgProcessor extends EventEmitter {
     }
 
     // special case - handle block number 1
-    if(foundDifference) {
+    if (foundDifference) {
       forks.push(0);
     }
 
     return forks;
+  }
+
+  /**
+   * The opposite of the accumulate done in BlocksAdder
+   * @param {*} params - all params are database entries
+   */
+  async calcAddressAssetsPerTx({ inputs, outputs, tx } = {}) {
+    const { addresses, assets } = calcAddressesAssets({ inputs, outputs });
+
+    // remove data from Addresses, Assets
+    const promisesAddress = []; // do parallel work
+    addresses.forEach((assets, address) => {
+      // upsert address/asset
+      const assetIds = Object.keys(assets);
+      assetIds.forEach((asset) => {
+        promisesAddress.push(
+          (async () => {
+            const dbAddress = await addressesDAL.findByAddressAsset({
+              address,
+              asset,
+              transaction: this.dbTransaction,
+            });
+            await dbAddress.update(
+              {
+                inputSum: new Decimal(dbAddress.inputSum).minus(assets[asset].inputSum).toString(),
+                outputSum: new Decimal(dbAddress.outputSum)
+                  .minus(assets[asset].outputSum)
+                  .toString(),
+                balance: new Decimal(dbAddress.balance)
+                  .minus(assets[asset].outputSum.minus(assets[asset].inputSum))
+                  .toString(),
+                txsCount: new Decimal(dbAddress.txsCount).minus(1).toString(),
+              },
+              { transaction: this.dbTransaction }
+            );
+          })()
+        );
+      });
+    });
+
+    // the addresses are needed to calculate the Asset.keyholders
+    await Promise.all(promisesAddress);
+
+    const promisesAsset = [];
+    // Assets
+    assets.forEach((value, asset) => {
+      promisesAsset.push(
+        (async () => {
+          const [keyholders, dbAsset] = await Promise.all([
+            addressesDAL.count({
+              where: {
+                [Op.and]: {
+                  asset,
+                  balance: {
+                    [Op.gt]: 0,
+                  },
+                },
+              },
+              transaction: this.dbTransaction,
+            }),
+            assetsDAL.findById(asset, {
+              transaction: this.dbTransaction,
+            }),
+          ]);
+          const issued =
+            asset !== '00'
+              ? value.issued
+              : tx.index === 0 // get reward once per block
+              ? new Decimal(
+                  tx.blockNumber === 1
+                    ? new Decimal(this.genesisTotalZp).times(100000000)
+                    : calcRewardByHeight(tx.blockNumber)
+                )
+              : new Decimal(0);
+          await dbAsset.update(
+            {
+              issued: new Decimal(dbAsset.issued).minus(issued).toString(),
+              destroyed: new Decimal(dbAsset.destroyed).minus(value.destroyed).toString(),
+              outstanding: new Decimal(dbAsset.outstanding)
+                .minus(issued.minus(value.destroyed))
+                .toString(),
+              keyholders,
+              txsCount: new Decimal(dbAsset.txsCount).minus(1).toString(),
+            },
+            { transaction: this.dbTransaction }
+          );
+        })()
+      );
+    });
+    await Promise.all(promisesAsset);
+
+    // remove data from contracts
+    const promisesContract = [];
+    // addresses are unique
+    addresses.forEach((value, address) => {
+      if (address.startsWith('c')) {
+        promisesContract.push(
+          (async () => {
+            const contract = await contractsDAL.findByAddress(address, {
+              transaction: this.dbTransaction,
+            });
+            if (contract) {
+              await contract.update(
+                { txsCount: new Decimal(contract.txsCount).minus(1).toString() },
+                { transaction: this.dbTransaction }
+              );
+            }
+          })()
+        );
+      }
+    });
+    // assets are unique
+    assets.forEach((value, asset) => {
+      // check only is an asset was minted
+      if (value.issued.gt(0)) {
+        promisesContract.push(
+          (async () => {
+            const contractId = asset.substring(0, 72);
+            const contract = await contractsDAL.findById(contractId, {
+              transaction: this.dbTransaction,
+            });
+            if (contract) {
+              const assetsIssued = await assetsDAL.count({
+                where: {
+                  asset: {
+                    [Op.like]: `${contractId}%`,
+                  },
+                },
+                transaction: this.dbTransaction,
+              });
+              await contract.update({ assetsIssued }, { transaction: this.dbTransaction });
+            }
+          })()
+        );
+      }
+    });
+
+    await Promise.all(promisesContract);
   }
 }
 
--- worker/jobs/blocks/test/ReorgProcessor.db.test.js
+++ worker/jobs/blocks/test/ReorgProcessor.db.test.js
@@ -3,6 +3,12 @@
 const test = require('blue-tape');
 const truncate = require('../../../../test/lib/truncate');
 const blocksDAL = require('../../../../server/components/api/blocks/blocksDAL');
+const txsDAL = require('../../../../server/components/api/txs/txsDAL');
+const outputsDAL = require('../../../../server/components/api/outputs/outputsDAL');
+const inputsDAL = require('../../../../server/components/api/inputs/inputsDAL');
+const addressesDAL = require('../../../../server/components/api/addresses/addressesDAL');
+const assetsDAL = require('../../../../server/components/api/assets/assetsDAL');
+const addressTxsDAL = require('../../../../server/components/api/address-txs/addressTxsDAL');
 const NetworkHelper = require('../../../lib/NetworkHelper');
 const ReorgProcessor = require('../ReorgProcessor');
 const createDemoBlocksFromTo = require('../../../../test/lib/createDemoBlocksFromTo');
@@ -33,6 +39,102 @@ test('ReorgProcessor.doJob() (DB)', async function (t) {
     const hashes = allBlocks.map((block) => block.hash);
     t.assert(!hashes.includes(badHash), `${given}: db should not have the bad hash`);
   });
+  await wrapTest('Given a reorg and address balances', async (given) => {
+    const address = 'zen1qmwpd6utdzqq4lg52c70f6ae5lpmvcqtm0gvaxqlrvxt79wquf38s9mrydf';
+    const reorgProcessor = getReorgProcessor();
+    // create demo blocks
+    const badHash = 'bad';
+    await createDemoBlocksFromTo(1, 6, badHash);
+    await createDemoBlocksFromTo(7, 10);
+    // add demo txs
+    let txBlock6;
+    let outputBlock1;
+    for (let i = 1; i <= 10; i++) {
+
+      const tx = await txsDAL.create({
+        blockNumber: i,
+        index: 0,
+        hash: 'aaa' + i,
+      });
+      const output = await outputsDAL.create({
+        blockNumber: i,
+        txId: tx.id,
+        index: 0,
+        lockType: 'Coinbase',
+        address,
+        asset: '00',
+        amount: 1,
+      });
+      await addressTxsDAL.create({
+        blockNumber: i,
+        txId: tx.id,
+        address,
+      });
+      if (i === 1) {
+        outputBlock1 = output;
+      }
+      if(i === 6) {
+        txBlock6 = tx;
+      }
+    }
+
+    // add addresses data in block 6
+    await inputsDAL.create({
+      blockNumber: 6,
+      txId: txBlock6.id,
+      outputId: outputBlock1.id,
+      index: 0,
+      isMint: false,
+      lockType: 'Coinbase',
+      address,
+      asset: '00',
+      amount: 1,
+    });
+
+    await addressesDAL.create({
+      address,
+      asset: '00',
+      inputSum: '1',
+      outputSum: '10',
+      balance: '9',
+      txsCount: '2',
+    });
+    await assetsDAL.create({
+      asset: '00',
+      issued: '2000045000000000',
+      destroyed: '0',
+      outstanding: '2000045000000000',
+      keyholders: '1',
+      txsCount: '10',
+    });
+
+    await reorgProcessor.doJob();
+    const addressDb = await addressesDAL.findOne({ where: { address, asset: '00' } });
+    const assetDb = await assetsDAL.findOne({ where: { asset: '00' } });
+    const addressTxsDb = await addressTxsDAL.findAll({ where: { address } });
+    t.equal(
+      addressDb.balance,
+      '5',
+      `${given}: should revert the balance to the state before the reorg`
+    );
+    t.equal(addressTxsDb.length, 5, `${given}: should delete all AddressTxs from the fork`);
+    t.assert(addressTxsDb.every(a => a.blockNumber < 6), `${given}: should leave AddressTxs before the fork`);
+    t.equal(
+      assetDb.issued,
+      '2000020000000000',
+      `${given}: should revert asset.issued to the state before the reorg`
+    );
+    t.equal(
+      assetDb.outstanding,
+      '2000020000000000',
+      `${given}: should revert asset.outstanding to the state before the reorg`
+    );
+    t.equal(
+      assetDb.txsCount,
+      '5',
+      `${given}: should revert asset.txsCount to the state before the reorg`
+    );
+  });
 });
 
 async function wrapTest(given, test) {
@@ -50,5 +152,5 @@ function getReorgProcessor() {
       },
     });
   };
-  return new ReorgProcessor(networkHelper);
+  return new ReorgProcessor(networkHelper, '20000000');
 }
