9a33a03572b2568234c9e5bc5e3c66bef52c408a
Lior Frenkel <lior@zenprotocol.com>
2021-01-12 12:07:30 +0200

    refactor: fetch naming json periodically

--- server/lib/getContractName.js
+++ server/lib/getContractName.js
@@ -1,7 +1,11 @@
 const axios = require('axios');
 const Config = require('../config/Config');
+const logger = require('../lib/logger');
 
 let data = null;
+let lastFetchTime = null;
+const FETCH_GAP_TIME = 12 * 60 * 60 * 1000; // 12 hours
+const TRY_REFETCH_EVERY = 20 * 60 * 1000; // 20 minutes
 
 /**
  * Get a name by contract ID
@@ -12,6 +16,7 @@ let data = null;
 module.exports = function get(id) {
   if (!data) {
     fetchJson();
+    lastFetchTime = Date.now();
     return '';
   }
   return data[id];
@@ -19,8 +24,10 @@ module.exports = function get(id) {
 
 async function fetchJson() {
   try {
+    logger.info('try fetch naming json...');
     const result = await axios.default.get(Config.get('CONTRACT_NAMING_JSON'), { timeout: 10000 });
     if(result.status === 200) {
+      logger.info('fetched naming json successfully');
       // convert to a hash table
       data = result.data.items.reduce((hash, item) => {
         return Object.assign(hash, item);
@@ -32,4 +39,15 @@ async function fetchJson() {
 }
 
 // fetch on init
+lastFetchTime = Date.now();
 fetchJson();
+
+// fetch periodically
+function refetch() {
+  if(Date.now() - lastFetchTime > FETCH_GAP_TIME) {
+    lastFetchTime = Date.now();
+    fetchJson();
+  }
+  setTimeout(refetch, TRY_REFETCH_EVERY);
+}
+refetch();
